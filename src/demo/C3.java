c55da2b968ea8970f62e5ad6fb368141b571ba0a
alt-sidkishan <whistler.dummyemail@gmail.com>
2023-05-19 09:42:47 +0530

    Modified C3 class

--- src/demo/C3.java
+++ src/demo/C3.java
@@ -3,5 +3,6 @@ package demo;
 public class C3 {
 	public static void main(String[] args) {
 		System.out.println("just some random console output from C3 class");
+		System.out.println("some new changes after merging this file and verified");
 	}
 }
