ccd35de813c87bf2adaacacbdffd7ab0c1da7197
alt-sidkishan <whistler.dummyemail@gmail.com>
2023-05-19 09:55:46 +0530

    Modified C3 class from branch B3 on same line

--- src/demo/C3.java
+++ src/demo/C3.java
@@ -3,6 +3,6 @@ package demo;
 public class C3 {
 	public static void main(String[] args) {
 		System.out.println("just some random console output from C3 class");
-		System.out.println("some new changes after merging this file and verified");
+		System.out.println("changes from same line from branch B3");
 	}
 }
